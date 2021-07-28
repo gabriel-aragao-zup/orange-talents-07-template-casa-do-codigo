0d4e77531e37b9c90501e6ade9bf1e874d15fcc1
gabriel-aragao-zup <gabriel.batista@zup.com.br>
2021-07-28 15:43:26 -0300

    initial commit

---
+++ src/main/java/br/com/zup/codehouse/CodehouseApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.codehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodehouseApplication.class, args);
+	}
+
+}
